17ca35dba073e79deae11a12874646c8d010a0cc
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-20 10:29:15 +0530

    Create ForL.java

---
+++ ForL.java
@@ -0,0 +1,12 @@
+public class Forl{
+  public static void main(String[] args) {
+
+        for(int i=1;i<=10;i=i-2){
+            System.out.println("Hello World : "+i);
+        }
+        for(int i=15;i>15;i--){
+            System.out.println("Hello World : "+i);
+        }
+    // Correct This Errors
+    }
+}
