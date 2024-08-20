b581032f2856d26eeab1f0e97fd500b48493e57c
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-20 10:34:23 +0530

    Create DoWhileL.java

---
+++ DoWhileL.java
@@ -0,0 +1,12 @@
+public class DoWhileL{
+public static void main(String[]args){
+int docount=7;
+        do{
+            System.out.println("Do While "+docount);
+        docount--;
+        }
+        while(docount>=15);
+    }
+  // Print 7 to 15 Number at doCount
+}
+}
